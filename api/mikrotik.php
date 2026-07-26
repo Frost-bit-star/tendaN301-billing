e50e75a0be2904a633f2c4969c6ba3141cedba69
Frost-bit-star <morganmilstone983@gmail.com>
2026-07-26 12:55:36 +0300

    fix: remove :do block from wireless set, keep ssid with quotes outside block

--- api/mikrotik.php
+++ api/mikrotik.php
@@ -130,8 +130,8 @@ function generateProvisionScript($db, $routerId, $name, $wireguardIP, $deviceId)
     $script .= ":do { /interface bridge add comment=\"Jasiri WiFi Bridge\" name=jasiri-bridge } on-error={}\n\n";
 
     $script .= "# --- Wireless AP (open, no password) ---\n";
-    $script .= ":do { /interface wireless set [find] mode=ap-bridge wireless-protocol=none ssid=\"Jasiri WiFi\" frequency=auto disabled=no } on-error={}\n";
-    $script .= ":do { /interface wireless security-profiles set [find name=default] authentication-types=none unicast-cast-encryption=none } on-error={}\n";
+    $script .= "/interface wireless set [find] mode=ap-bridge wireless-protocol=none ssid=\"Jasiri WiFi\" frequency=auto disabled=no\n";
+    $script .= "/interface wireless security-profiles set [find name=default] authentication-types=none unicast-cast-encryption=none\n";
     $script .= ":do { /interface bridge port remove [find interface=wlan1] } on-error={}\n";
     $script .= ":do { /interface bridge port add bridge=jasiri-bridge interface=wlan1 } on-error={}\n";
     $script .= ":do { /interface bridge port remove [find interface=ether2] } on-error={}\n";
