97b239df1596a4d3cd7b6c0dced7b40bb10e9ffd
Frost-bit-star <morganmilstone983@gmail.com>
2026-07-26 13:10:11 +0300

    fix: wrap all wireless commands in on-error so they dont block provisioning script

--- api/mikrotik.php
+++ api/mikrotik.php
@@ -130,9 +130,9 @@ function generateProvisionScript($db, $routerId, $name, $wireguardIP, $deviceId)
     $script .= ":do { /interface bridge add comment=\"Jasiri WiFi Bridge\" name=jasiri-bridge } on-error={}\n\n";
 
     $script .= "# --- Wireless AP (open, no password) ---\n";
-    $script .= "/interface wireless set [find] mode=ap-bridge frequency=auto disabled=no\n";
-    $script .= "/interface wireless security-profiles set [find name=default] authentication-types=none unicast-cast-encryption=none\n";
-    $script .= "/interface wireless set [find] ssid=JasiriWiFi\n";
+    $script .= ":do { /interface wireless set [find] mode=ap-bridge frequency=auto disabled=no } on-error={}\n";
+    $script .= ":do { /interface wireless security-profiles set [find name=default] authentication-types=none } on-error={}\n";
+    $script .= ":do { /interface wireless set [find] ssid=JasiriWiFi } on-error={}\n";
     $script .= ":do { /interface bridge port remove [find interface=wlan1] } on-error={}\n";
     $script .= ":do { /interface bridge port add bridge=jasiri-bridge interface=wlan1 } on-error={}\n";
     $script .= ":do { /interface bridge port remove [find interface=ether2] } on-error={}\n";
